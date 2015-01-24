aec6ce2bbc660296ea7c238f2103b0597f9019f7
Takuo Doi <takuo.doi@iclound.com>
2015-01-24 15:04:06 +0900

    initial

---
+++ src/main/java/jp/topse/nosql/weka/App.java
@@ -0,0 +1,13 @@
+package jp.topse.nosql.weka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
